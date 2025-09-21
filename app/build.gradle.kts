2328db2572c9f2cb4af91d6334ef33969d7466e4
Wicia <michalvietecha@gmail.com>
2025-09-21 19:12:24 +0200

    Domains - introducing extra icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "pl.hexmind.mindshaper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "MVP_1_0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
